e5972fdce8f8de0e4c5d9c8eabada75b2068dd44
aRusenov <bobpodesta@yahoo.in>
2017-02-08 23:15:56 +0200

    Install LeakCanary

--- app/src/main/java/com/twobonkers/hungry/HApplication.java
+++ app/src/main/java/com/twobonkers/hungry/HApplication.java
@@ -2,6 +2,7 @@ package com.twobonkers.hungry;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
 import com.twobonkers.hungry.presentation.dagger.app.AppComponent;
 import com.twobonkers.hungry.presentation.dagger.app.BaseModule;
 import com.twobonkers.hungry.presentation.dagger.app.DaggerAppComponent;
@@ -17,6 +18,14 @@ public class HApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+
+        LeakCanary.install(this);
+
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
         }
